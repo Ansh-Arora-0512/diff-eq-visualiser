603b05ce12e8f0e243299d4892c746ad7434307d
the-one-who-asked <ansh.arora0512@gmail.com>
2025-01-16 15:55:09 +0100

    sum shit

--- frontend/src/components/Keyboard.js
+++ frontend/src/components/Keyboard.js
@@ -1,42 +1,41 @@
-import { React, useState } from 'react';
+import { React } from 'react';
 import { EditableMathField, StaticMathField } from 'react-mathquill';
 import { useEq } from './EqContext';
 
 function EqInput() {
-  const { mathFieldRef, typeText } = useEq();
-  const [eq, setEq] = useState("");
+  const { mathFieldRef, writeLatex, eq, setEq } = useEq();
+  const blacklisted = ["\\", " ", "{", "}"];
+  const braces = ["(", "["];
 
-  const operands = {
-    "+": ["{}+{}", 3],
-    "-": ["{}-{}", 3],
-    "*": ["{}\\cdot{}", 2],
-    "^": ["{}^{}"],
-    "(": ["\\left({}\\right.", 0],
-    ")": ["\\left.{}\\right)", 0],
-  }
-
-  function onEdit(mathField) {
-    setEq(mathField.latex());
+  function keyPress(mathField, event) {
+    const key = event.key;
+    prev_is_char = false;
+    if (blacklisted.includes(key)) {
+      event.preventDefault();
+    }
+    if (braces.includes(key)) {
+      mathField.cmd(key);
+      writeLatex("{}", false, 1);
+      event.preventDefault;
+    }
   }
 
   return (
-    <div className="Eq">
-      <EditableMathField
-        ref={mathFieldRef}
-        latex={eq}
-        onChange={onEdit}
-      />
-      <p>{eq}</p>
-    </div>
+    <EditableMathField
+      ref={mathFieldRef}
+      latex={eq}
+      onKeyDown={keyPress}
+      onChange={(mathField)=>{setEq(mathField.latex())}}
+    />
   )
 }
 
-function CalcKey({ expr }) {
-  const { mathFieldRef, typeText } = useEq();
+function CalcKey({ expr, isCmd=false, lcount=0, display=undefined }) {
+  const { mathFieldRef, writeLatex, eq, setEq } = useEq();
 
   return (
-    <button onClick={() => typeText(expr)}>
-      <StaticMathField>{expr}</StaticMathField>
+    <button onClick={() => writeLatex(expr, isCmd, lcount)}>
+      <StaticMathField>{display ? display : expr}</StaticMathField>
     </button>
   )
 }
@@ -44,30 +43,23 @@ function CalcKey({ expr }) {
 function Keyboard() {
   return (
     <div className="Keyboard">
-      <table id="Variables">
-        <tr></tr>
-        <tr></tr>
-        <tr></tr>
-        <tr></tr>
-      </table>
       <table id="Operators">
         <tr>
-          <td><CalcKey expr={"\\frac{d}{dx}"}></CalcKey></td>
-          <td><button><MathJax hideUntilTypeset='first'>{"$ \\frac{d}{dx} $"}</MathJax></button></td>
-          <td><button><MathJax hideUntilTypeset='first'>{"$ \\frac{\\partial}{\\partial x} $"}</MathJax></button></td>
-          <td><button><MathJax hideUntilTypeset='first'>{"$ ( $"}</MathJax></button></td>
-          <td><button><MathJax hideUntilTypeset='first'>{"$ ) $"}</MathJax></button></td>
+          <td><CalcKey expr={"\\frac{d}{d{}}{}"} lcount={1}></CalcKey></td>
+          <td><CalcKey expr={"\\frac{\\partial}{\\partial{}}{}"} lcount={1}></CalcKey></td>
+          <td><CalcKey expr={"\\left({}\\right)"} lcount={3} display={"("}></CalcKey></td> // test lcount = 2
+          <td><CalcKey expr={")"} isCmd={true}></CalcKey></td>
         </tr>
         <tr>
-          <td><button><MathJax hideUntilTypeset='first'>{"$ x^2 $"}</MathJax></button></td>
-          <td><button><MathJax hideUntilTypeset='first'>{"$ x^n $"}</MathJax></button></td>
-          <td><button><MathJax hideUntilTypeset='first'>{"$ \\sqrt{x} $"}</MathJax></button></td>
-          <td><button><MathJax hideUntilTypeset='first'>{"$ \\sqrt[n]{x} $"}</MathJax></button></td>
+          <td><CalcKey expr={"{}^{2}"} lcount={5} display={"x^2"}></CalcKey></td>
+          <td><CalcKey expr={"{}^{}"} lcount={4} display={"x^n"}></CalcKey></td>
+          <td><CalcKey expr={"\\sqrt{}"} lcount={1} display={"\\sqrt{x}"}></CalcKey></td>
+          <td><CalcKey expr={"\\sqrt[]{}"} lcount={3} display={"\\sqrt[n]{x}"}></CalcKey></td> // test lcount = 2
         </tr>
         <tr>
           <td><button><MathJax hideUntilTypeset='first'>{"$ \\pi $"}</MathJax></button></td>
-          <td><button><MathJax hideUntilTypeset='first'>{"$ \\ln{x} $"}</MathJax></button></td>
-          <td><button><MathJax hideUntilTypeset='first'>{"$ \\log{x} $"}</MathJax></button></td>
+          <td><CalcKey expr={"\\ln{}"} lcount={1} display={"\\ln{x}"}></CalcKey></td>
+          <td><CalcKey expr={"\\log{}"} lcount={1} display={"\\sqrt{x}"}></CalcKey></td>          <td><button><MathJax hideUntilTypeset='first'>{"$ \\log{x} $"}</MathJax></button></td>
           <td><button><MathJax hideUntilTypeset='first'>{"$ \\log_n{x} $"}</MathJax></button></td>
         </tr>
         <tr>
